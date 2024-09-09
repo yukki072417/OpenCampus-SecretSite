3c9fe617508fd9065706b6fdc4ca382555a09b21
yukki072417 <yukki072417@gmail.com>
2024-09-10 07:47:11 +0900

    fix code

--- login.js
+++ login.js
@@ -1,28 +1,41 @@
-// you can setting id and password
+// You can setting id and password
 const id = "sample"
 const password = "sample";
 
+// You can building show and hide function
+function showPass() {
+    let showingPassIconPath = 'assets/icons/passwordShowing.png'
+    let hidingPassIconPath = 'assets/icons/passwordHiding.png'
+
+    const $icon = $('.show-pass');
+    const currentType = $('#user-password').attr('type');
+
+    if (currentType === 'password') {
+        passwordShowing($icon,showingPassIconPath);
+    } else {
+        passwordHiding($icon,hidingPassIconPath);
+    }
+
+}
 
 
 function login() {
-   check();
-   
-    if(check() == true){
-        move();
+    // When login user, checking password and id
+    check();
+    if(checking() == true){
+        movingSite();
     }else{
         alert('パスワードかIDを間違えています');
     }
 
 }
 
-
-
 // you can not rewriting below code 
 
-const move = () => {
+function movingSite() {
     return location.href = "./your-site/site.html";
 }
-function check(){
+function checking(){
  
     let entered_id = document.getElementById("user-id");
     let entered_password = document.getElementById("user-password");
@@ -30,4 +43,12 @@ function check(){
     if(entered_id.value == id && entered_password.value == password){
         return true
     }
+}
+function passwordShowing(icon,iconPath){
+    $('#user-password').attr('type', 'text');
+    icon.attr('src', iconPath);
+}
+function passwordHiding(icon,iconPath){
+    $('#user-password').attr('type', 'password');
+    icon.attr('src', iconPath);
 }
